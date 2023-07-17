d168c11eb5fba86a13756265f11d95f688038730
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-07-18 00:06:56 +0200

    Using main_frame to include main content
    passing 'page_content' as variable from Authentication servlet to jsp
    
    This is the trickiest part:
    <jsp:include page="<%=pagePath%>" ></jsp:include>

--- src/main/java/acme/back/servlet/Authentification.java
+++ src/main/java/acme/back/servlet/Authentification.java
@@ -15,16 +15,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 public class Authentification extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 0L;
-	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		
+
 		HttpSession session = (HttpSession)request.getSession();
 		session.removeAttribute("erreur");
 		String pageApresErreur = "/jsp/erreur.jsp";
-		
-		//On a besoin d'un tag unique pour être sur de passer dans le bon if 
+
+		//On a besoin d'un tag unique pour être sur de passer dans le bon if
 		//C'est a cela que sert la ligne dans la jsp : <input name="connexion" type="submit" value="connexion"/>
 		try {
 			if ("connexion".equals(request.getParameter("connexion"))) {
@@ -32,16 +32,18 @@ public class Authentification extends HttpServlet {
 				AuthentificationBean ab = new AuthentificationBean();
 				//login
 				ab.setLogin(request.getParameter("login"));
-				
+
 				//password que l'on hash (Cela devrait se faire dans la jsp mais je n'y arrive pas !)
 				ab.setPassword(Utilitaire.getStringMD5(request.getParameter("password")));
 				ab = AuthentificationService.getService().authenticate(ab);
 				System.out.println("ab : " + ab);
 				//Pour communiquer entre le serveur et le client on passe par la session qui est une hashmap (cle/valeur)
 				//on choisit arbitrairement une cle (string et on lui associe un objet que l'on pourra récuperer dans la jsp)
+				request.setAttribute("page_name", "Acme Station");
 				session.setAttribute("authentification", ab);
-				getServletConfig().getServletContext().getRequestDispatcher("/jsp/blanc.jsp").forward(request, response);			
-			} else 
+				request.setAttribute("page_content", "content_datatable");
+				getServletConfig().getServletContext().getRequestDispatcher("/jsp/blanc.jsp").forward(request, response);
+			} else
 			//Je viens du menu deconnexion
 			if ("menuDeconnexion".equals(request.getParameter("parametre"))) {
 				pageApresErreur = "/login.jsp";
@@ -57,7 +59,7 @@ public class Authentification extends HttpServlet {
 				getServletConfig().getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
 			} else {
 				getServletConfig().getServletContext().getRequestDispatcher("/jsp/erreur.jsp").forward(request, response);
-			}			
+			}
 		}
 		catch(BizException be) {
 			try {
