f6373c82cdeb85e3f6a9ed1b881df8fe1e4c42e7
avilacarlos <138768599+avilacarlos@users.noreply.github.com>
2023-07-26 12:31:03 +0200

    Suppression javax. Validation acces list utilisateurs

--- src/main/java/acme/back/servlet/Utilisateur.java
+++ src/main/java/acme/back/servlet/Utilisateur.java
@@ -3,15 +3,19 @@ package acme.back.servlet;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
 
 import acme.back.service.UtilisateurService;
 import acme.front.UtilisateurBean;
 import acme.util.BizException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 /**
